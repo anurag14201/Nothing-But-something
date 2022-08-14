3071fe95dda679549e0d43bc067a48f7f445ac0d
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-08-14 11:41:06 +0600

    something

--- javascript/summery.js
+++ javascript/summery.js
@@ -2,3 +2,15 @@ console.log('I am javascrpit for my file');
 
 const blogs = document.getElementsByTagName('p');
 console.log(blogs);
+
+// const friend = document.getElementsById('Friends');
+// console.log(Friends.children);
+const friend = document.getElementById('Friends');
+// console.log(friend.children);
+const fifth = friend.children[4];
+console.log(fifth);
+friend.removeChild(fifth);
+
+const friends = document.createElement('li');
+friends.innerText = 'Friend-11'
+friend.appendChild(friends)
